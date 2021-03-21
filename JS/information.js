faf6064c7f07ac9276f54920acba27eb782653a0
bayan-alkhatib <bayankhatibtr@gmail.com>
2021-03-21 14:19:42 +0200

    prep4 completed

--- JS/information.js
+++ JS/information.js
@@ -1,1 +1,22 @@
 'use strict';
+let vaccineName=['Sputnik V ','AstraZeneca','mRNA-1273','BNT162b2'];
+let companyName=['Gamaleya Research Institute','Oxford University and AstraZeneca','moderna ',' pfizer and BioNTech'];
+let countryofManufact=['Russian',]
+function Vaccine (vaccineName,companyName,countryofManufact,tempreture,dose, effectivenessRate,imgPath,price){
+  this.vaccineName=vaccineName;
+  this.companyName=companyName;
+  this.countryofManufact=countryofManufact;
+  this.tempreture=tempreture;
+  this.dose=dose;
+  this.effectivenessRate=effectivenessRate;
+  this.imgPath=imgPath;
+  this.price=price;
+  Vaccine.vaccineData.push(this);
+}
+
+Vaccine.vaccineData=[];
+
+for(let i=0;i<vaccineName.length;i++){
+new  Vaccine()
+}
+
--- README.md
+++ README.md
@@ -18,6 +18,14 @@
     - Admin page that shows each registration, and the quantity of vacines stock.
     - About us page.
 
+# Project Wireframes:
+[Home-Page](./img/wireframes/HomepageWireFrame.png), [Information-Page](./img/wireframes/informationPageWireFrame.png), 
+[Registration-Page](./img/wireframes/RegistrationWireFrame.png), 
+[OrderTrack-Page](./img/wireframes/OrderTrackingPage.png), 
+[Aboutus-Page](./img/wireframes/AboutUsPage.png)
+
+# Project Management Tool:
+[Trello](https://trello.com/b/Ew3YAWV3/vaccines-registration-plan)
 # Semantic Versioning:
 *daily record: >>>> guide line >>>> 1.0.2 >>>>> 1 ---> major, 0 ---> minar, 2 ---> patch (bug fix)*
 

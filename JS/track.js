a8a7b6431c93d06a5ab47bfb16608d4aecab7d33
bayan-alkhatib <bayankhatibtr@gmail.com>
2021-03-24 09:58:32 +0200

    changes trackjs

--- JS/track.js
+++ JS/track.js
@@ -13,7 +13,6 @@ function submitHolder(event){
   event.preventDefault();
   let userSelected = event.target.user_type_list.value;
   let Oredernumber = event.target.id.value;
-
   if(parseInt(userSelected)===0){
     alert('Please select type of user');
   }else if(parseInt(userSelected)===1){
